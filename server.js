0ff25dea25d32d494c318ddd624793bbebe84422
KS Durazo <sdurazo.UNLV@gmail.com>
2023-06-24 18:30:01 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // DEPENDENCIES
 const express = require('express')
+const morgan = require('morgan')
 
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+// Middleware should be added above your routes.  Here, we are setting up which view engine will be used and requiring JSX so we can utilize it build our views.
 
 // ROUTES
 app.get('/', (req, res) => {
---
+++ views/layout/default.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+
+function Default (html) {
+    return (
+        <html>
+            <head>
+                <title>Default</title>
+            </head>
+            <body>
+                <h1>HTML Rendered!</h1>
+                <div className='container'>
+                    {html.children}
+                    {/* When using {}, we are telling React/JSX to stop, evaluate what is inside, and then render the contents. */}
+                </div>
+            </body>
+        </html>
+    )
+}
+
+module.exports = Default
