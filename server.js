613156c4e6cc4791897df7f91f467deb74331e4d
KS Durazo <sdurazo.UNLV@gmail.com>
2023-06-25 11:33:14 -0700

    create and new routes complete

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 // Middleware should be added above your routes.  Here, we are setting up which view engine will be used and requiring JSX so we can utilize it build our views.
 
 // ROUTES
@@ -19,14 +20,14 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
+// Breads
+const breadsController = require('./controllers/bread_controller.js')
+app.use('/breads', breadsController)
+
 // 404 Page
 app.get ('*', (req, res) => {
     res.send('404')
 })
-
-// Breads
-const breadsController = require('./controllers/bread_controller.js')
-app.use('/breads', breadsController)
 
 // LISTEN
 app.listen(PORT, () => {
